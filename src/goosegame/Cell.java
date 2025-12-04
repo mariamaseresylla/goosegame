3f0ab10f038aa0b7ff545e5fd849502f905ebd0c
Sylla Mariama Sere <mariama-sere.sylla.etu@a12p2.fil.univ-lille.fr>
2025-12-04 16:30:07 +0100

    update

--- src/goosegame/Cell.java
+++ src/goosegame/Cell.java
@@ -1,6 +1,13 @@
 package goosegame;
 
 public abstract class Cell {
+    protected int n;
+    protected Player pion;
+    public Cell(int n){
+        this.n= n;
+        this.pion=null;
+    }
 
+    
     
 } 
