b8e583a6f4d2a4dc9d4c14f2b4ee5d2690dfda48
Mariama Sere Sylla <mariama-sere.sylla.etu@univ-lille.fr>
2025-12-10 11:30:46 +0100

    ecruture de classe

--- src/goosegame/cell/CellTeleport.java
+++ src/goosegame/cell/CellTeleport.java
@@ -1,14 +1,19 @@
 package goosegame.cell;
 import goosegame.*;
+/*class CellTeleport */
 public class CellTeleport extends Cell {
     private int nextNum;
-    public CellTeleport(int numCell,int nextNum){
+    public CellTeleport(int numCell, int nextNum){
         super(numCell);
-        this.nextNum=nextNum;
+        this.nextNum= nextNum;
     }
-    
+    /**
+     * calcul the rebond of the cell
+     * @param dee
+     * @return
+     */
     public int bound(int dee){
-        return  this.nextNum - this.numCell ;
-
+       return  this.nextNum- this.numCell;
+       
     }
 }
