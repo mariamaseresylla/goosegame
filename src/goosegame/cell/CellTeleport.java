a17d13fdfe1ec1550313684a7e4f6f774c31df03
Mariama Sere Sylla <mariama-sere.sylla.etu@univ-lille.fr>
2025-12-10 11:06:44 +0100

    ecriture de classes cell

--- src/goosegame/cell/CellTeleport.java
+++ src/goosegame/cell/CellTeleport.java
@@ -1,6 +1,14 @@
 package goosegame.cell;
 import goosegame.*;
 public class CellTeleport extends Cell {
-
+    private Cell nextCell;
+    public CellTeleport(int numCell,Cell nextCell){
+        super(numCell);
+        this.nextCell=nextCell;
+    }
     
+    public int bound(int dee){
+        return  this.nextCell.getNumCell() - this.numCell ;
+
+    }
 }
