38fadd50f84c1d80e4fea4aa5b4ce4f04eea5145
Mariama Sere Sylla <mariama-sere.sylla.etu@univ-lille.fr>
2025-12-10 11:38:30 +0100

    documentation

--- src/goosegame/Cell.java
+++ src/goosegame/Cell.java
@@ -1,8 +1,11 @@
 package goosegame;
-
+//class cell
 public abstract class Cell {
+    //numero of cell
     protected int numCell;
+    //the player
     protected Player player;
+    //built the cell
     public Cell(int numCell){
         this.numCell= numCell;
         this.player=null;
@@ -13,20 +16,44 @@ public abstract class Cell {
      public int getNumCell() {
         return numCell;
     }
+
+    /**
+     * return the player of the cell
+     * @return
+     */
     public Player getPlayer() {
         return player;
     }
+
+    /**
+     * modifie the player of the cell
+     * @param player the player of the cell 
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
+
+    /**
+     * celcul de bound of the cell 
+     * @param dee the dee of the cell 
+     * @return 
+     */
     public int  bound(int dee){
         return 0;
 
     }
-  
+    /**
+     * check if the player can leave the cell
+     * @return if the player can leave 
+     */
     public boolean canLeave(){
         return true;
     }
+
+    /**
+     * represantation of the cell
+     * @return the representation of the cell
+     */
     public String toSting(){
         return "je suis la cellule numero :"+this.numCell+"i containt :"+this.player;
     }
--- src/goosegame/cell/CellTeleport.java
+++ src/goosegame/cell/CellTeleport.java
@@ -2,7 +2,9 @@ package goosegame.cell;
 import goosegame.*;
 /*class CellTeleport */
 public class CellTeleport extends Cell {
+    //number of nextcell
     private int nextNum;
+    //built Cellteleport
     public CellTeleport(int numCell, int nextNum){
         super(numCell);
         this.nextNum= nextNum;
