1b37d42e6557f43d55cf808a995c8389c3a35ec2
Mariama Sere Sylla <mariama-sere.sylla.etu@univ-lille.fr>
2025-12-12 15:52:56 +0100

    classical board

--- src/goosegame/Board.java
+++ src/goosegame/Board.java
@@ -1,16 +1,20 @@
 package goosegame;
-
+//class Board 
 public abstract class Board {
     protected final int nbOfCells ;
     protected Cell [] theCells;
-
+    //build board 
     public Board( int nbOfCells){
         this.nbOfCells = nbOfCells;
         this.theCells= new Cell[nbOfCells];
         this.initBoard();
           
         }
-    
+    /**
+     * returns the cell at the position of index.
+     * @param index
+     * @return
+     */
     public Cell getCell(int index){
         return this.theCells[index];
 
@@ -21,10 +25,6 @@ public abstract class Board {
         return this.nbOfCells;
     }
     protected void initBoard(){
-        for (int i=0; i<this.theCells.length;i++){
-           
-           
-
-        }
+        
     }
 }
--- src/goosegame/ClassicalBoard.java
+++ src/goosegame/ClassicalBoard.java
@@ -1,8 +1,54 @@
 package goosegame;
 
+import goosegame.cell.CellGoose;
+import goosegame.cell.CellTeleport;
+import goosegame.cell.CellTrap;
+
 public class ClassicalBoard extends Board {
     public ClassicalBoard(int nbOfCells){
         super(nbOfCells);
+    }
+
+    protected void initBoard(){
+        for ( int i=0; i< this.theCells.length;i++){
+            if (i==9){
+                this.theCells[i]= new CellGoose(i);
+            }
+            if (i==18){
+                this.theCells[i]= new CellGoose(i);
+            }
+            if (i==27){
+                this.theCells[i]= new CellGoose(i);
+            }
+            if (i==36){
+                this.theCells[i]= new CellGoose(i);
+            }if (i==45){
+                this.theCells[i]= new CellGoose(i);
+            }
+            if (i==54){
+                this.theCells[i]= new CellGoose(i);
+            }
+            if (i==31){
+                this.theCells[i]= new CellTrap(i);
+            }
+             if (i==52){
+                this.theCells[i]= new CellTrap(i);
+            }
+             if (i==6){
+                this.theCells[i]= new CellTeleport(i,i+1);
+            }
+
+             if (i==42){
+                this.theCells[i]= new CellTeleport(i,i+1);
+            }
+             if (i==58){
+                this.theCells[i]= new CellTeleport(i,i+1);
+            }
+            else {
+                
+            }
+
+        }
     }
      
 }
