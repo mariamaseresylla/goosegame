2629d75f27f23965cb91e9f286c993b64711fda2
Mariama Sere Sylla <mariama-sere.sylla.etu@univ-lille.fr>
2025-12-19 22:39:06 +0100

    hg

--- src/goosegame/GameMain.java
+++ src/goosegame/GameMain.java
@@ -2,15 +2,15 @@ package goosegame;
 
 public class GameMain{
     public static void main(String[] args) {
-        Board b=new ClassicalBoard();
-        Game g=new Game(b);
-        Player p1=new Player("alpha");
-        Player p2=new Player("pathe");
-        g.addPlay(p2);
-        g.addPlay(p1);
+        if (args.length < 2) {
+            System.out.println("Usage: java -jar goosegame.jar <joueur1> <joueur2> ...");
+            return;
+        }
+        Board b = new ClassicalBoard();
+        Game g = new Game(b);
+        for (String name : args) {
+            g.addPlay(new Player(name));
+        }
         g.play();
-
-
-
     }
 }
